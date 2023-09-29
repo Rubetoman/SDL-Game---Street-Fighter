5c77636d78be7ae4db7e126abd5759ad76a7458e
Humberto Dias <hldtux@gmail.com>
2023-09-29 13:30:28 -0300

    -std=c++17

--- src/ModuleWindow.cpp
+++ src/ModuleWindow.cpp
@@ -42,11 +42,6 @@ bool ModuleWindow::Init()
 			LOG("Window could not be created! SDL_Error: %s\n", SDL_GetError());
 			ret = false;
 		}
-		else
-		{
-			//Get window surface
-			screen_surface = SDL_GetWindowSurface(window);
-		}
 	}
 
 	return ret;
